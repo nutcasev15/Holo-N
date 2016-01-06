74b0a6b5e6e5b0107888830a9ba74d8dfd67c447
eoghan2t9 <eoghan61@gmail.com>
2016-01-06 01:14:52 +0000

    Linux 3.10.86
    Conflicts:
    	sound/usb/quirks-table.h

--- mm/memory.c
+++ mm/memory.c
@@ -3533,7 +3533,6 @@ static int do_linear_fault(struct mm_struct *mm, struct vm_area_struct *vma,
 	/* The VMA was not fully populated on mmap() or missing VM_DONTEXPAND */
 	if (!vma->vm_ops->fault)
 		return VM_FAULT_SIGBUS;
-
 	return __do_fault(mm, vma, address, pmd, pgoff, flags, orig_pte);
 }
 
@@ -3748,7 +3747,6 @@ int handle_pte_fault(struct mm_struct *mm,
 			if (vma->vm_ops)
 				return do_linear_fault(mm, vma, address,
 						pte, pmd, flags, entry);
-
 			return do_anonymous_page(mm, vma, address,
 					pte, pmd, flags);
 		}
