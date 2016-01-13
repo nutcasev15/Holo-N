aee9ea3337c32707b1cd337e4f581ed931319978
eoghan2t9 <eoghan61@gmail.com>
2016-01-13 21:34:31 +0000

    Optimize KSM
    Change-Id: I0ea041e06d3d027eee6ca10f21d271aae676abde

--- mm/ksm.c
+++ mm/ksm.c
@@ -218,10 +218,10 @@ static unsigned long ksm_pages_unshared;
 static unsigned long ksm_rmap_items;
 
 /* Number of pages ksmd should scan in one batch */
-static unsigned int ksm_thread_pages_to_scan = 100;
+static unsigned int ksm_thread_pages_to_scan = 250;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 20;
+static unsigned int ksm_thread_sleep_millisecs = 1500;
 
 static int ksm_should_sleep = 1;
 static void ksm_wakeup_thread(unsigned long nothing);
