ec7c0ecebb08cbc5bf2ee28dac8fef2b7866a06d
nutcasev15 <tab.andsupp@gmail.com>
2017-06-24 20:26:22 +0530

    [PM] Hardware Wakelock Suppressor v2

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -640,6 +640,10 @@ void __pm_wakeup_event(struct wakeup_source *ws, unsigned int msec)
 
 	wakeup_source_report_event(ws);
 
+	// Regulate Wake Time
+	if (ktime_to_ms(ws->total_time) > 900000 || ws->active_count > 8)
+		msec = ((msec * 3) / 5);
+
 	if (!msec) {
 		wakeup_source_deactivate(ws);
 		goto unlock;
