428e2f10502143921fe900575ed93423a519fb59
nutcasev15 <tab.andsupp@gmail.com>
2017-08-20 16:04:10 +0530

    Linux v3.10.107
    From kernel.org :
    
    https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/commit/?h=v3.10.107&id=0ee88216358c4a1821022763cb84d2b0550bf1e2
    
    through
    
    https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/commit/?h=v3.10.107&id=0e3d5747a3ef9986d7617cec396850bf9d039309
    
    Allow stack to grow up to address space limit
    mm: larger stack guard gap, between vmas
    
    Above 2 commits reverted due to SIGSEV in libart.so trying to start zygote, causing a bootloop.

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1301,7 +1301,9 @@ clock_set:
 			return;
 		}
 		timeout--;
-		udelay(10);
+		spin_unlock_irq(&host->lock);
+		usleep_range(900, 1100);
+		spin_lock_irq(&host->lock);
 	}
 
 	clk |= SDHCI_CLOCK_CARD_EN;
