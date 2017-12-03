1447aa6bebd457d5152ad37d9bd890568d051a6c
nutcasev15 <tab.andsupp@gmail.com>
2017-12-03 09:05:47 +0530

    [SCHED] Drop the Wakeup Latency
    Change-Id: I3d13a19a068cc9ff5c790fb256c0db09ef11d21c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -87,10 +87,10 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity = 400000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity = 400000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 200000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
